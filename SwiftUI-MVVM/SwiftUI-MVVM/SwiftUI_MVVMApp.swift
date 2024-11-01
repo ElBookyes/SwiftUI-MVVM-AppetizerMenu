7e3c35a1069474374d9cd84e801dd015db5bd088
Yordan Tuparov <80248189+ElBookyes@users.noreply.github.com>
2024-11-01 13:58:44 +0200

    Project Commit

---
+++ SwiftUI-MVVM/SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,12 @@
+
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerListView()
+        }
+    }
+}
---
+++ SwiftUI-MVVM/SwiftUI-MVVM/Utilities/APError.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case unableToComplete
+    case invalidResponse
+    case invalidData
+}
